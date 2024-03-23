d74c966636cbcde53a0f9982770ce803771d08d7
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-03-23 12:30:42 +0500

    finish

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = prompt("ismingizni kriting");
+let year11 = +prompt("hozirgi yilni kriting");
+let year22 = +prompt("tug'ilgan yilinigizni kriting");
+
+while (isNaN(year11) || year11 == 0) {
+    year22 = +prompt("hozirgi yilni kritishingiz kerak");
+}
+while (isNaN(year22) || year22 == 0) {
+    year22 = +prompt("tug'ilgan yilinigizni kritishingiz kerak");
+}
+function calcul(year1 = year11,year2 = year22) {
+    let answer = year1 - year2
+    console.log(`${name} , yoshingiz ${answer}`);
+}
+calcul()
